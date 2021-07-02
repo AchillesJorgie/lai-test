786c963a6d03260979e0160bfbf0c1d2d1c487ab
Jorg <jorgie@ruangguru.com>
2021-07-02 19:13:56 +0700

    add AI

--- side/lai-test/src/App.tsx
+++ side/lai-test/src/App.tsx
@@ -5,8 +5,8 @@ import "./App.css";
 
 const ROW_COLUMN_DEFAULT_SIZE = 3;
 
-const setNewBoard = () => {
-  const newBoard = [];
+let setNewBoard = () => {
+  let newBoard = [];
   for (let col = 0; col < ROW_COLUMN_DEFAULT_SIZE; col++) {
     let newRow = [];
     for (let row = 0; row < ROW_COLUMN_DEFAULT_SIZE; row++) {
@@ -19,49 +19,91 @@ const setNewBoard = () => {
 
 function App() {
   let [boardArray, setBoardArray] = useState(setNewBoard());
-  let [gameState, setGameState] = useState("ongoing");
   let [playerTurn, setPlayerTurn] = useState("o");
   let [xRowColumnCount, setXRowColumnCount] = useState([0, 0]);
   let [oRowColumnCount, setORowColumnCount] = useState([0, 0]);
   let [winningPlayer, setWinningPlayer] = useState("");
 
-  const onClick = async (player, row, column) => {
-    const updatedBoard = [...boardArray];
+  let onClick = async (player, row, column) => {
+    let updatedBoard = [...boardArray];
     if (updatedBoard[column][row].length > 0 || winningPlayer.length > 0) {
       return;
     }
     updatedBoard[column][row] = player;
     if (playerTurn === "o") {
       await setORowColumnCount([
-        oRowColumnCount[0] + column,
-        oRowColumnCount[1] + row,
+        oRowColumnCount[0] + column + 1,
+        oRowColumnCount[1] + row + 1,
       ]);
     } else {
       await setXRowColumnCount([
-        xRowColumnCount[0] + column,
-        xRowColumnCount[1] + row,
+        xRowColumnCount[0] + column + 1,
+        xRowColumnCount[1] + row + 1,
       ]);
     }
     await setBoardArray(updatedBoard);
     await setPlayerTurn(playerTurn === "o" ? "x" : "o");
   };
 
+  // let getWinningTile = (c, r) => {
+  //   let board = [...boardArray];
+  //   let winningTiles = [];
+  //   for (let col = 0; col < ROW_COLUMN_DEFAULT_SIZE; col++) {
+  //     for (let row = 0; row < ROW_COLUMN_DEFAULT_SIZE; row++) {
+  //       if (c === col && r !== row && board[col][row] === "") {
+  //         winningTiles.push([col, row]);
+  //       }
+  //       if (c !== col && r === row && board[col][row] === "") {
+  //         winningTiles.push([col, row]);
+  //       }
+  //       if (col === row && c !== col && r !== row && board[col][row] === "") {
+  //         winningTiles.push([col, row]);
+  //       }
+  //       if (
+  //         c + r === 2 &&
+  //         c !== col &&
+  //         r !== row &&
+  //         col + row === 2 &&
+  //         board[col][row] === ""
+  //       ) {
+  //         winningTiles.push([col, row]);
+  //       }
+  //     }
+  //   }
+  //   return winningTiles;
+  // };
+
   useEffect(() => {
     if (
       xRowColumnCount[0] % 3 === 0 &&
       xRowColumnCount[1] % 3 === 0 &&
-      xRowColumnCount[0] + xRowColumnCount[1] !== 0
+      xRowColumnCount[0] + xRowColumnCount[1] !== 0 &&
+      (xRowColumnCount[0] === 6 || xRowColumnCount[1] === 6)
     ) {
       setWinningPlayer("x");
     }
     if (
       oRowColumnCount[0] % 3 === 0 &&
       oRowColumnCount[1] % 3 === 0 &&
-      oRowColumnCount[0] + oRowColumnCount[1] !== 0
+      oRowColumnCount[0] + oRowColumnCount[1] !== 0 &&
+      (oRowColumnCount[0] === 6 || oRowColumnCount[1] === 6)
     ) {
       setWinningPlayer("o");
     }
   }, [xRowColumnCount, oRowColumnCount]);
+
+  // useEffect(() => {
+  //   if (playerTurn === "x") {
+  //     let winningTiles = getWinningTile();
+  //     let randomIndex =
+  //       winningTiles[Math.floor(Math.random() * winningTiles.length)];
+  //     if (winningPlayer !== "") {
+  //       setTimeout(function () {
+  //         onClick(playerTurn, randomIndex[0], randomIndex[1]);
+  //       }, 1500);
+  //     }
+  //   }
+  // }, [playerTurn]);
 
   return (
     <div className="App">
